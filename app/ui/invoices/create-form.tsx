1d3618d13efb54ac0cb3e5bdac1262e22fad04c2
A Mad Dev <muhammedaadhilv69@gmail.com>
2026-08-22 11:56:00 +0300

    FIX: create-form TypeError

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -16,7 +16,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
     const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
